d9126b88e36605ff0bbd200e4f0a630e0e23eae0
Rokaya-Maged <rokaya.magedh@gmail.com>
2025-10-17 16:39:51 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public static void main(String[] args) {
-
-    com.example.os.commands.ass1.Parser parser = new com.example.os.commands.ass1.Parser();
-    parser.parse("pwd");
-    com.example.os.commands.ass1.Terminal terminal = new com.example.os.commands.ass1.Terminal(parser);
-    terminal.chooseCommandAction();
-
-}
